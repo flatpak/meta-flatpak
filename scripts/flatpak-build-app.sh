7274391b31adac41d923dbe7dceb4552529f6ca6
Krisztian Litkey <krisztian.litkey@intel.com>
2017-02-02 15:51:23 +0200

    WiP: working on sample app-build script.
    Signed-off-by: Krisztian Litkey <krisztian.litkey@intel.com>

--- scripts/flatpak-build-app.sh
+++ scripts/flatpak-build-app.sh
@@ -2,9 +2,13 @@
 
 # Print help on usage.
 print_usage () {
+    local _ec="${1:-1}"
+    shift
+
     if [ -n "$*" ]; then
         echo "$*"
     fi
+
     echo "usage: $0 -n <name> [-s <src> -c <cmd> [other options]]"
     echo "    <name>     application id, flatpak name will be org.flatpak.<name>"
     echo "    <src>      sub-directory containing the application source"
@@ -22,92 +26,169 @@ print_usage () {
     echo "    tar -xvzf zsh-5.2.tar.gz"
     echo "    $0 --name zsh --source zsh-5.2 --command zsh \\"
     echo "       --share network,ipc --filesystem home"
+
+    exit $_ec
 }
 
 # Parse the command line.
 parse_command_line () {
     while [ -n "$1" ]; do
         case $1 in
+            --arch)
+                ARCH="$2"
+                shift 2
+                ;;
+
+            # options for the SDK to use
+            --sdk)
+                SDK_NAME="$2"
+                shift 2
+                ;;
+
+            --sdk-version)
+                SDK_VERSION="$2"
+                shift 2
+                ;;
+
+            # options for the application to build
+            --org|-o)
+                APP_ORG="$2"
+                shift 2
+                ;;
+
             --name|-n)
-                FLATPAK_APP="$2"
+                APP_NAME="$2"
                 shift 2
                 ;;
+
             --src|--source|-s)
-                FLATPAK_SRC="$2"
+                APP_SRC="$2"
                 shift 2
                 ;;
-            --command|-c)
+
+            --clone|-C)
+                APP_URI="$2"
+                shift 2
+                ;;
+
+            # options for flatpak metadata
+            --app-version)
+                APP_VERSION="$2"
+                shift 2
+                ;;
+
+            --command|--cmd|-c)
                 shift
-                while [ -n "$1" ]; do
-                    [ -n "$FLATPAK_CMD" ] && FLATPAK_CMD="$FLATPAK_CMD $1" || \
-                            FLATPAK_CMD="$1"
+                while [ -n "$1" -a "$1" != "--" ]; do
+                    [ -n "$APP_CMD" ] && APP_CMD="$APP_CMD $1" || APP_CMD="$1"
                     shift
                 done
+                [ "$1" = "--" ] && shift || :
                 ;;
-            --version|-v)
-                FLATPAK_VER="$2"
+
+            --share)
+                APP_SHARE="$APP_SHARE --share=$2"
                 shift 2
                 ;;
-            --share|-S)
-                FLATPAK_SHARE="$FLATPAK_SHARE --share=$2"
+
+            --filesystem|--fs)
+                APP_FS="$APP_FS --filesystem=$2"
                 shift 2
                 ;;
-            --filesystem|--fs|-F)
-                FLATPAK_FS="$FLATPAK_FS --filesystem=$2"
+
+            # options for the flatpak repo to export to
+            --repo)
+                APP_REPO="$2"
                 shift 2
                 ;;
-            --repo|-R)
-                FLATPAK_REPO="$2"
+
+            --gpg-home|--gpg-homedir)
+                GPG_HOME="$2"
                 shift 2
                 ;;
-            --clone|-C)
-                FLATPAK_CLONE="$2"
+
+            --gpg-key|--gpg-id)
+                GPG_ID="$2"
                 shift 2
                 ;;
+
             --help|-h)
-                print_usage
-                exit 0
+                print_usage 0
                 ;;
 
             *)
-                print_usage "unknown argument $1"
-                exit 1
+                print_usage 1 "unknown argument $1"
                 ;;
             esac
     done
 
-    if [ -z "$FLATPAK_APP" ]; then
-        print_usage "missing application name (--name <app>)"
+    if [ -z "$APP_NAME" ]; then
+        print_usage 1 "missing application name (--name <app>)"
+    fi
+
+    if [ -z "$ARCH" ]; then
+        ARCH=$(uname -m)
+    fi
+
+    if [ -z "$APP_REPO" ]; then
+        APP_REPO="$(pwd)/$APP_NAME.flatpak"
+    fi
+
+    if [ -z "$APP_SRC" ]; then
+        APP_SRC=$APP_NAME
+    fi
+
+    if [ -z "$APP_CMD" ]; then
+        APP_CMD=$APP_NAME
+    fi
+
+    if [ -z "$APP_ORG" ]; then
+        APP_ORG="${SDK_NAME%.BaseSdk}"
+    fi
+
+    if [ -n "$GPG_ID" ]; then
+        if [ -n "$GPG_HOME" ]; then
+            GPG_SIGN="--gpg-homedir=$GPG_HOME --gpg-sign=$GPG_ID"
+        else
+            GPG_SIGN="--gpg-sign=$GPG_ID"
+        fi
+    fi
+
+    builddir="$(pwd)/.build.$APP_NAME"
+    RUNTIME_NAME="${SDK_NAME%.BaseSdk}.BasePlatform"
+}
+
+# clone application sources
+clone_sources () {
+    if [ -e $APP_SRC ]; then
+        return 0
+    fi
+
+    if [ ! -n "$APP_URI" ]; then
+        echo "error: no sources found ($APP_SRC) and no app URI given."
         exit 1
     fi
 
-    if [ -z "$FLATPAK_SRC" ]; then
-        FLATPAK_SRC=$FLATPAK_APP
-    fi
-
-    if [ -z "$FLATPAK_CMD" ]; then
-        FLATPAK_CMD=$FLATPAK_APP
-    fi
-
-    builddir=".$FLATPAK_APP.builddir"
+    echo "* Cloning $APP_URI to $APP_SRC..."
+    git clone $APP_URI $APP_SRC
 }
 
 # Initialize the flatpak build directory.
 flatpak_init () {
-    echo "* Initializing flatpak build directory..."
+    echo "* Initializing flatpak build directory ($builddir) for $APP_NAME..."
 
     rm -fr $builddir && mkdir $builddir
     flatpak build-init $builddir \
-        org.flatpak.$FLATPAK_APP \
-        org.yocto.BaseSdk org.yocto.BasePlatform \
-        $FLATPAK_VER
+        $APP_ORG.$APP_NAME \
+        $SDK_NAME $RUNTIME_NAME \
+        $APP_VERSION
 }
 
 # Configure the application for flatpak build.
 flatpak_configure () {
-    echo "* Configuring flatpak build..."
+    echo "* Configuring $APP_NAME for flatpak build..."
 
-    cd $FLATPAK_SRC
+    cd $APP_SRC
 
     if [ ! -f configure ]; then
         if [ -x autogen.sh ]; then
@@ -118,9 +199,9 @@ flatpak_configure () {
     fi
 
     if [ -x configure ]; then
-        flatpak build ../$builddir ./configure --prefix=/app
+        flatpak build $builddir ./configure --prefix=/app
     elif [ ! -f Makefile ]; then
-        echo "Don't know how to configure this package..."
+        echo "error: don't know how to configure this package..."
         exit 1
     fi
 
@@ -129,29 +210,29 @@ flatpak_configure () {
 
 # Build the application for/with flatpak.
 flatpak_build () {
-    echo "* Performing flatpak build..."
+    echo "* Building $APP_NAME for flatpak exporting..."
 
-    cd $FLATPAK_SRC
+    cd $APP_SRC
 
-    flatpak build ../$builddir make
+    flatpak build $builddir make
 
     cd ..
 }
 
 # Install the application for/with flatpak.
 flatpak_install () {
-    echo "* Performing flatpak install..."
+    echo "* Performing flatpak install for $APP_NAME..."
 
-    cd $FLATPAK_SRC
+    cd $APP_SRC
 
-    flatpak build ../$builddir make install
+    flatpak build $builddir make install
 
     cd ..
 }
 
 # Resolve libraries missing from the runtime image.
 flatpak_resolve_libs () {
-    echo "* Resolving libraries for flatpak app..."
+    echo "* Resolving libraries for $APP_NAME..."
 
     flatpak build $builddir ./scripts/resolve-libs.sh -v \
         -P runtime.libs -R / -F $builddir/files
@@ -159,14 +240,13 @@ flatpak_resolve_libs () {
 
 # Finalize the flatpak build of the application.
 flatpak_finish () {
-    echo "* Finalizing flatpak build..."
-    echo "* share: $FLATPAK_SHARE"
-    echo "* filesystems: $FLATPAK_FS"
-    echo "* command: $FLATPAK_CMD"
+    echo "* Finalizing flatpak build of $APP_NAME..."
+    echo "*   namespaces: $APP_SHARE"
+    echo "*   filesystems: $APP_FS"
+    echo "*   command: $APP_CMD"
 
     flatpak build-finish $builddir \
-        $FLATPAK_SHARE $FLATPAK_FS \
-        --command="$FLATPAK_CMD"
+        $APP_SHARE $APP_FS --command="$APP_CMD"
 }
 
 # Export the application to the given repository.
@@ -204,27 +284,23 @@ flatpak_export () {
 
     [ -n "$EDITOR" ] && $EDITOR $_m || vi $_m
 
-
-    flatpak build-export $FLATPAK_SIGN $FLATPAK_REPO $builddir
-}
-
-# Clone the app sources from the given repository.
-clone_sources () {
-    if [ ! -e $FLATPAK_SRC ]; then
-        echo "* Cloning $FLATPAK_CLONE into $FLATPAK_SRC..."
-        git clone $FLATPAK_CLONE $FLATPAK_SRC
-    elif [ -n "$FLATPAK_CLONE" ]; then
-        echo "* $FLATPAK_SRC already exists, *not* cloning $FLATPAK_CLONE..."
-    fi
+    flatpak build-export $GPG_SIGN $APP_REPO $builddir
 }
 
 
 #########################
 # main script
-FLATPAK_REPO="flatpak.repo"
-FLATPAK_VER="0.0.1"
-FLATPAK_SIGN="--gpg-homedir=$(pwd)/.gpg.flatpak --gpg-sign=repo-signing@key"
-FLATPAK_CMD=""
+SDK_NAME="iot.refkit.BaseSdk"
+SDK_ARCH="x86_64"
+SDK_VERSION="latest-build"
+
+APP_NAME=""
+APP_VERSION="latest-build"
+APP_REPO=""
+
+GPG_ID="iot-ref-kit@key"
+GPG_HOME=""
+GPG_SIGN=""
 
 set -e
 
@@ -235,30 +311,11 @@ flatpak_init
 flatpak_configure
 flatpak_build
 flatpak_install
-flatpak_resolve_libs
+#flatpak_resolve_libs
 flatpak_finish
 flatpak_export
 
 echo ""
-echo "$FLATPAK_APP exported to the repository as org.flatpak.$FLATPAK_APP."
+echo "$APP_NAME exported to $APP_REPO $APP_ORG.$APP_NAME version $APP_VERSION."
 
 exit 0
-
-#rm -fr $builddir && mkdir $builddir
-#flatpak build-init $builddir org.flatpak.$name \
-#    org.yocto.BaseSdk org.yocto.BasePlatform $version
-#
-#cd $dir
-#flatpak build ../$builddir ./configure --prefix=/app
-#flatpak build ../$builddir make
-#flatpak build ../$builddir make install
-#cd ..
-#
-#flatpak build $builddir ./resolve-libs.sh -v -P runtime.libs \
-#                                             -R / -F $builddir/files
-#
-#flatpak build-finish $builddir \
-#    --share=$SHARED --filesystem=$FILESYSTEMS --command=$command
-#flatpak build-export flatpak.repo $builddir
-#
-
